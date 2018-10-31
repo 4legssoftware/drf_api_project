da19aabae8e8d964fd189ad78f17b94e008f3946
robpark <robert.d.park@gmail.com>
2018-10-31 09:58:42 -0400

    basically runnable

---
+++ README.md
@@ -0,0 +1,7 @@
+# Django Rest Framework API Project Reference
+## Overview
+This a simple sample app repo for the purposes of experimentation and sharing of examples.
+## Requirements
+* Python (3.7)
+## Running
+`python manage.py runserver`
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'drf_api_project.urls'
+ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
     {
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
