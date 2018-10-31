d5d0bf1e71098b619aaa75537a3a0bc073ce7722
robpark <robert.d.park@gmail.com>
2018-10-31 14:13:36 -0400

    refactor out getting the api health

--- drf_api/monitoring/views.py
+++ drf_api/monitoring/views.py
@@ -4,15 +4,20 @@ from rest_framework.response import Response
 from . import Health
 from . import serializers
 
-checks = {
-    1: Health(id=1, status=serializers.HEALTH_STATUS_HEALTHY),
-}
-
 
 class HealthCheckViewSet(viewsets.ViewSet):
     serializer_class = serializers.HealthSerializer
 
     def list(self, _request):
         serializer = serializers.HealthSerializer(
-            instance=checks.values(), many=True)
+            instance=self.health_check().values(), many=True)
         return Response(serializer.data)
+
+    def health_check(self):
+        checks = {
+            1: Health(id=1, status=self.get_the_api_health_status()),
+        }
+        return checks
+
+    def get_the_api_health_status(self):
+        return serializers.HEALTH_STATUS_HEALTHY
