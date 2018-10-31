2aed14f9ae477acb4fabbf16fdfae6455d32e586
robpark <robert.d.park@gmail.com>
2018-10-31 11:01:42 -0400

    using deprecated list_route, but return basic non-model response

--- drf_api/monitoring/views.py
+++ drf_api/monitoring/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HealthCheckViewSet(viewsets.GenericViewSet):
+
+    @list_route(methods=['get'], permission_classes=[])
+    def check(self, _request):
+        data = {
+            'health': 'healthy'
+        }
+        return Response(data)
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from rest_framework import routers
+from drf_api.monitoring import views
 
 router = routers.DefaultRouter()
+router.register(r'health', views.HealthCheckViewSet, basename='health')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
